b06898db67389544313084a7e406b470c56980cf
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-10 13:32:32 -0600

    Student model tests passing

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Student, type: :model do
+  describe 'validations' do
+    it 'is invalid without all attributes' do
+      student_1 = Student.create()
+
+      expect(student_1).to_not be_valid
+    end
+    it 'is valid with all attributes' do
+      student_1 = Student.create(name: "Bobby Hill")
+
+      expect(student_1).to be_valid
+    end
+  end
+end
