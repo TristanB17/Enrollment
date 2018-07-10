24c082a7395fab74407e567d664c9e1e0fbfcbe4
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-10 15:29:11 -0600

    Migrated addresses into students, changed config route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'students#index'
-  resources :students
+  resources :students do
+    resources :addresses
+  end 
 end
