dda96311e40d3eadd4ec309d5d49fd6ccf889dca
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-10 14:14:11 -0600

    Successful debugging

--- spec/features/students/user_can_create_student_spec.rb
+++ spec/features/students/user_can_create_student_spec.rb
@@ -11,17 +11,17 @@ describe 'user' do
       expect(page).to have_button("Create Student")
     end
     it 'creates a student' do
-      student_1 = Student.create(name: "Bobby Hill")
+      name = "Bobby Hill"
 
       visit new_student_path
       greeting = "Create new student"
 
-      fill_in :student_name, with: student_1.name
+      fill_in :student_name, with: name
 
       click_button "Create Student"
 
-      expect(current_path).to eq("/students/#{student_1.id}")
-      expect(page).to have_content(student_1.name)
+      expect(current_path).to eq("/students/#{Student.all.first.id}")
+      expect(page).to have_content(name)
     end
   end
 end
