9976b4d032632bb68b1147a08cfe7fa9e2fbf5ee
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-10 13:54:40 -0600

    new student controller methods and tests added

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,23 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.save
+      redirects_to student_path(student)
+    else
+      render :new
+    end 
+  end
+
+  private
+
+  def student_params
+    params.require(:students).permit(:name)
+  end
 end
---
+++ spec/features/students/user_can_create_student_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'user' do
+  describe 'visits a create student page' do
+    it 'sees all appropriate fields' do
+      visit new_student_path
+      greeting = "Create new student"
+
+      expect(page).to have_field(:student_name)
+      expect(page).to have_content(greeting)
+      expect(page).to have_button("Create Contact")
+    end
+  end
+end
